047e9ade4165afdb762f4a0a0564e5dca2ce289a
Shrejal Sharma <iluminati5372@gmail.com>
2025-10-26 17:40:12 +0530

    var

--- aloobhujia.js
+++ aloobhujia.js
@@ -1,1 +1,10 @@
-console.log('aloobhujia');
+const accountId = 1009923
+let accountEmail = "aloo@gmail.com";
+var accountPassword = "12345";
+let accountcity = "BHOPAL";
+let accountstate = "MADHYA PRADESH";
+
+console.table([accountId, accountEmail, accountPassword,accountcity,accountstate]);
+
+// my acc number is 3 
+console.log('my account is in', accountstate);
