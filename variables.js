7c0ca2268cc3741c2e8ee1e3054ca06c34a436e4
Shrejal Sharma <iluminati5372@gmail.com>
2025-11-05 19:57:56 +0530

    conversion

--- variables.js
+++ variables.js
@@ -3,3 +3,13 @@ console.log("Hello Worlf");
 console.log("aloo"); 
 console.log("aloost");
 console.log("alooobhai"); 
+ let score = undefined
+ console.log(typeof score);
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber);  
+
+let isloggedIn = 0;
+console.log(isloggedIn);
+let BooleanIsLoggedIn = Boolean(isloggedIn);
+console.log(BooleanIsLoggedIn);
